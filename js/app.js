b24675b61e383bf9eedf043842079c6cbb02b6d3
rcbdelarosa <rcb.delarosa@gmail.com>
2020-12-06 23:32:51 -0600

    created html storyline and set-up for modals, buttons

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-console.log('$');
+// console.log('$');
+
+// ***** sudo, self steps ***** //
+/* 
+Step 1. In html add background -- done. However, I want the image to be all in the page and locked in place. How can i do this?
+Step 2. Write storyline into html (Modals, Buttons, etc)
+Step 3. Create modal, that shows up upon loading page (figure out how to add images within modal)
+Step 4. In HTML, write DIVs for each house, add sigil image. 
+Step 5. Add css standard items 
+*/
+
+// ***** game set-up ***** //
+
+// ***** jquery ***** //
+
+$(() => {
+  /* game code */
+});
