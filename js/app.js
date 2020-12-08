9ad94786acf15154909a5552bd7b7e52d2fd1a12
rcbdelarosa <rcb.delarosa@gmail.com>
2020-12-07 19:47:16 -0600

    fixing the picture location and aligment as per what Arthur shared

--- js/app.js
+++ js/app.js
@@ -3,10 +3,10 @@
 // ***** sudo, self steps ***** //
 /* 
 Step 1. In html add background -- done. However, I want the image to be all in the page and locked in place. How can i do this?
-Step 2. Write storyline into html (Modals, Buttons, etc)
-Step 3. Create modal, that shows up upon loading page (figure out how to add images within modal)
-Step 4. In HTML, write DIVs for each house, add sigil image. 
-Step 5. Add css standard items 
+Step 2. Write storyline into html (Modals, Buttons, etc) -- done 
+Step 3. Create modal, that shows up upon loading page (figure out how to add images within modal) -- done, went with button to initialize 
+Step 4. In HTML, write DIVs for each house, add sigil image -- done 
+Step 5. Add css standard items -- done 
 */
 
 // ***** game set-up ***** //
@@ -25,26 +25,50 @@ function playMusic() {
 // ***** jquery ***** //
 $(() => {
   /* DOM cache */
+  // container for page
+  const $container = $('container');
 
   // story modal
-  const $modal = $('#modal-story'); // This is the story modal
-  const $texbox = $('#modal-textbox1'); // This is the textbox in the modal
-  const $openModal = $('#openModal-GameInit'); // This is the button that opens the modal
-  const $closedModal = $('#close1'); // this is the anchor tag that closes the modal
+  const $modal1 = $('#modal-story'); // This is the story modal
+  const $texbox1 = $('#modal-textbox1'); // This is the textbox in the modal
+  const $openModal1 = $('#openModal-GameInit'); // This is the button that opens the intro story modal
+  const $closedModal1 = $('#close1'); // this is the anchor tag that closes the modal
+
+  // game instructions button
+  const $openModal2 = $('#instructions'); // This is the button that opens the instruction modal
+
+  // instructions modal
+  const $modal2 = $('#modal-instructions'); // This is the story modal
+  const $texbox2 = $('#modal-textbox2'); // This is the textbox in the modal
+  const $closedModal2 = $('#close2'); // this is the anchor tag that closes the modal
 
   /* event handlers */
 
   // story modal
-  const openModal = (event) => {
-    $modal.css('display', 'flex');
+
+  // locking in container
+  $modal1.appendTo($container);
+  $modal2.appendTo($container);
+  const openModal1 = (event) => {
+    $modal1.css('display', 'flex');
   };
-  const closeModal = (event) => {
-    $modal.css('display', 'none');
+  const closeModal1 = (event) => {
+    $modal1.css('display', 'none');
+  };
+  // instructions modal
+  const openModal2 = (event) => {
+    $modal2.css('display', 'flex');
+  };
+  const closeModal2 = (event) => {
+    $modal2.css('display', 'none');
   };
 
   /* event listeners */
 
   // story modal
-  $openModal.on('click', openModal);
-  $closedModal.on('click', closeModal);
+  $openModal1.on('click', openModal1);
+  $closedModal1.on('click', closeModal1);
+  // instructions modal
+  $openModal2.on('click', openModal2);
+  $closedModal2.on('click', closeModal2);
 });
